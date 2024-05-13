1d8ec6a5fcd0be1156f05d9194807b8e5579bb68
dhruvpatel771 <dp3102003@gmail.com>
2024-05-13 14:17:10 +0530

    first commit

---
+++ api/socket/app.js
@@ -0,0 +1,48 @@
+import { Server } from "socket.io";
+import http from "http";
+
+
+const server = http.createServer();
+
+
+const io = new Server({
+  cors:{
+    origin: "https://soft-selkie-2e64e3.netlify.app/",
+    credentials: true
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+export default io;
